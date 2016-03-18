169f3ee78aae48468188f1a731981488d56a517e
Rana Das <rana.pratap.das@gmail.com>
2016-03-18 21:53:15 +0000

    Use java8 steam for array loop

--- src/main/java/com/rdas/ApplicationMain.java
+++ src/main/java/com/rdas/ApplicationMain.java
@@ -1,23 +1,24 @@
 package com.rdas;
 import java.util.Arrays;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+@Slf4j
 @SpringBootApplication
 public class ApplicationMain {
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(ApplicationMain.class, args);
 
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
+        log.info("Let's inspect the beans provided by Spring Boot:");
 
         String[] beanNames = ctx.getBeanDefinitionNames();
         Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
-    }
 
+        Arrays.asList(beanNames).stream().forEach(s -> System.out.println(s));
+
+    }
 }

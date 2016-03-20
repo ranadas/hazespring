43b6150c3b81bb81af7c4eb2701de84db000fd03
Rana Das <rana.pratap.das@gmail.com>
2016-03-20 08:52:47 +0000

    adding Dozer Mapping and Junut test.

---
+++ src/test/java/com/rdas/config/TestConfig.java
@@ -0,0 +1,13 @@
+package com.rdas.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * Created by rdas on 20/03/2016.
+ */
+@Configuration
+@Import(DozerConfiguration.class)
+public class TestConfig {
+
+}
